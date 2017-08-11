116308b9236effd5b648cb758e53e31a439150b8
Ankur Gupta <rukna1000@gmail.com>
2017-08-11 15:48:54 -0400

    Updated update script, added markdown snippets and modified i3 config

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 
 files=(
 	".vimrc" "vimrc"
-	".vim/UltiSnips" "vim/Ultisnips"
-	".config/i3" "config/i3"
-	".config/qutebrowser" "config/qutebrower")
+	".vim/UltiSnips" "vim/"
+	".config/i3" "config/"
+	".config/qutebrowser" "config/")
 
 for (( i=0;i<${#files[@]};i+=2 )) do
 	cp -r $HOME/${files[$i]} ${files[$i+1]}
