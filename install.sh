2febe5f2a0caa17e9dc45ae7d1ccb39f78340005
Ankur Gupta <rukna1000@gmail.com>
2017-08-12 01:07:21 -0400

    Updated install script

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ files=(
 	".vimrc" "vimrc"
 	".vim/" "vim/UltiSnips"
 	".config/" "config/i3"
-	".config/" "config/qutebrower")
+	".config/" "config/qutebrowser")
 
 for (( i=0;i<${#files[@]};i+=2 )) do
 	cp -r ${files[$i+1]} $HOME/${files[$i]}
