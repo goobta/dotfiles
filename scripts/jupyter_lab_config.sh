f670eaeb1d2b3b6873fc4ddf0381b56d8f7972f7
Ankur Gupta <rukna1000@gmail.com>
2019-11-27 23:05:19 -0500

    add comments and more jupyterlab extensions

--- scripts/jupyter_lab_config.sh
+++ scripts/jupyter_lab_config.sh
@@ -1,3 +1,16 @@
+# Git notebook linking
 pip install jupytext --upgrade
+jupyter nbextension install --py jupytext --user
+jupyter nbextension enable --py jupytext --user
+
+# Vim
 jupyter labextension install jupyterlab_vim
+
+# Visual file tree
 jupyter labextension install jupyterlab_filetree
+
+# Table of contents
+jupyter labextension install @jupyterlab/toc
+
+# Goto definition (Alt-click)
+jupyter labextension install @krassowski/jupyterlab_go_to_definition
